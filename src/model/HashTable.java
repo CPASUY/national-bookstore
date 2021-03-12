cf2221ecd9ff0674f29b7d23b8e5e1c8458ba718
anderson-154 <54720103+anderson-154@users.noreply.github.com>
2021-03-12 12:51:03 -0500

    added classes hash, interface and tuple

---
+++ src/model/HashTable.java
@@ -0,0 +1,35 @@
+package model;
+
+public class HashTable<K extends Comparable<K>, V> implements HashTableInterface<K,V> {
+
+	@Override
+	public int hash(K key, int explorator) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int auxHash(K key) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void put(Tuple<K, V> tuple) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public V get(K key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int keyToInteger(K key) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/model/HashTableInterface.java
@@ -0,0 +1,11 @@
+package model;
+
+public interface HashTableInterface<K extends Comparable<K>, V> {
+
+	public int hash(K key, int explorator);
+	public int auxHash(K key);
+	public void put(Tuple<K,V> tuple);
+	public V get(K key);
+	public int keyToInteger(K key);
+	
+}
