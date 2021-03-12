464bf2d1767ca342a7848ff936d3602dcdadf74e
Carolina Pasuy <carolinapasuy@hotmail.com>
2021-03-12 18:43:37 -0500

    FXML elements and queue class to develop the payment method

--- src/model/Queue.java
+++ src/model/Queue.java
@@ -1,29 +1,53 @@
 package model;
 
 public class Queue implements QueueInterface<Client> {
-
+	private QueuesNode<Client> front;
+	private QueuesNode<Client> last;
+	
+	public void setFront(QueuesNode<Client> front) {
+		this.front = front;
+	}
+	public QueuesNode<Client> getLast() {
+		return last;
+	}
+	public void setLast(QueuesNode<Client> last) {
+		this.last = last;
+	}
 	@Override
 	public void enqueue(Client c) {
-		
-		
+		QueuesNode<Client> newClient=new QueuesNode<Client>();
+		if(front==null) {
+			front=newClient;
+			last=newClient;
+		}
+		else {
+			newClient.setNext(last);
+			last=newClient;
+		}	
 	}
 
 	@Override
 	public Client dequeue() {
-		
-		return null;
+		QueuesNode<Client> e=last;
+		Client eliminated=e.getType();
+		last=last.getNext();
+		return eliminated;
 	}
 
 	@Override
 	public Client front() {
-		
-		return null;
+		Client f=front.getType();
+		return f;
 	}
 
 	@Override
 	public boolean empty() {
-		
-		return false;
+		if(front==null) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
 }
---
+++ src/ui/NationalLibraryController.java
@@ -0,0 +1,55 @@
+package ui;
+import java.io.IOException;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+
+public class NationalLibraryController {
+	private Stage stage;
+	
+	@FXML
+	private BorderPane basePane;
+	public NationalLibraryController(Stage s) {
+		stage=s;
+	}
+	public void initialize() {
+		stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			
+			@Override
+			public void handle(WindowEvent event) {
+				System.out.println("Closing the window!");
+			}
+		});
+
+	}
+	public void loadSeccion1() throws IOException {
+		FXMLLoader fxmload = new FXMLLoader(getClass().getResource("Seccion1.fxml"));
+		fxmload.setController(this);
+		Parent root;
+		root = fxmload.load();
+		basePane.getChildren().clear();
+		basePane.setCenter(root);
+	}
+	public void loadSeccion2() throws IOException{
+		FXMLLoader fxmload = new FXMLLoader(getClass().getResource("Seccion2.fxml"));
+		fxmload.setController(this);
+		Parent root;
+		root = fxmload.load();
+		basePane.getChildren().clear();
+		basePane.setCenter(root);
+	}
+	public void loadSeccion3() throws IOException{
+		FXMLLoader fxmload = new FXMLLoader(getClass().getResource("Seccion3.fxml"));
+		fxmload.setController(this);
+		Parent root;
+		root = fxmload.load();
+		basePane.getChildren().clear();
+		basePane.setCenter(root);
+	}
+}
