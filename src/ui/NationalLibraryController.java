8345060856309bdcda6e0ccb6d09ab3939f29e6a
Carolina Pasuy <carolinapasuy@hotmail.com>
2021-03-15 19:48:36 -0500

    Books table in the sesion 1 and create new atibutes in Book class

--- src/ui/NationalLibraryController.java
+++ src/ui/NationalLibraryController.java
@@ -1,14 +1,19 @@
 package ui;
 import java.io.IOException;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
-
+import model.Book;
 
 public class NationalLibraryController {
 	
@@ -16,6 +21,33 @@ public class NationalLibraryController {
 	
 	@FXML
 	private BorderPane basePane;
+
+    @FXML
+    private TableView<Book<String,Integer>> tableBooks;
+
+    @FXML
+    private TableColumn<Book<String,Integer>,String> idTitle;
+
+    @FXML
+    private TableColumn<Book<String,Integer>,String> idISBN;
+
+    @FXML
+    private TableColumn<Book<String,Integer>,Integer> idRack;
+
+    @FXML
+    private TableColumn<Book<String,Integer>,String> idChapter;
+
+    @FXML
+    private TableColumn<Book<String,Integer>,String> idReview;
+
+    @FXML
+    private TableColumn<Book<String,Integer>,String> idCritique;
+    @FXML
+    private TableColumn<Book<String,Integer>, Integer> idCost;
+
+    @FXML
+    private TableColumn<Book<String,Integer>,Integer> idQuantity;
+    
 	public NationalLibraryController(Stage s) {
 		stage=s;
 	}
@@ -53,4 +85,29 @@ public class NationalLibraryController {
 		basePane.getChildren().clear();
 		basePane.setCenter(root);
 	}
+	public void loadBooksList() {
+	    	basePane.setOnKeyPressed(null);
+	    	FXMLLoader fxmload = new FXMLLoader(getClass().getResource("Seccion1.fxml"));
+			fxmload.setController(this);
+			Parent root;
+			try {
+				root = fxmload.load();
+				basePane.getChildren().clear();
+				basePane.setCenter(root);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			tableBooks.getItems().clear();
+			//ObservableList<Book<String,Integer>>books= FXCollections.observableArrayList(gm.showList());
+			//tableBooks.setItems(books);
+			
+			idTitle.setCellValueFactory(new PropertyValueFactory<Book<String,Integer>, String>("Title"));
+			idISBN.setCellValueFactory(new PropertyValueFactory<Book<String,Integer>, String>("ISBN"));
+			idRack.setCellValueFactory(new PropertyValueFactory<Book<String,Integer>, Integer>("Rack"));
+			idChapter.setCellValueFactory(new PropertyValueFactory<Book<String,Integer>,String>("Chapter"));
+			idReview.setCellValueFactory(new PropertyValueFactory<Book<String,Integer>, String>("Review"));
+			idCritique.setCellValueFactory(new PropertyValueFactory<Book<String,Integer>, String>("Critique"));
+			idCost.setCellValueFactory(new PropertyValueFactory<Book<String,Integer>,Integer>("Review"));
+			idQuantity.setCellValueFactory(new PropertyValueFactory<Book<String,Integer>, Integer>("Critique"));
+		}
 }
